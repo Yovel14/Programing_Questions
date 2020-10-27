46125cbef4e3f72da07f9b0c22e592d232e19be8
Yovel14 <yovelavitan14@gmail.com>
2020-10-27 21:45:07 +0200

    finnished 34

--- Leetcode/Medium/34-Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ Leetcode/Medium/34-Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -31,7 +31,8 @@ Constraints:
 '''
 
 
-def searchRange(nums, target):#solution using binary search
+def searchRange(nums, target):
+    #solution using binary search
     def Find(a):#find possible ocurence of a
         length = len(nums)
         low = 0
@@ -49,3 +50,44 @@ def searchRange(nums, target):#solution using binary search
     FirstPos = Find(target)#find first occurence of target
     LastPos = Find(target+1)-1#find first occurence of target+1 and than remove one from the index to get last occurence of target
     return [FirstPos,LastPos] if FirstPos<=LastPos else [-1,-1]
+
+
+
+
+#Another way to solve is to do two binary searches to find first and last
+
+def searchRange(self, nums: List[int], target: int) -> List[int]:
+    low = 0
+    high = len(nums)-1
+    occurrence = -1
+    while low<=high: #find an occurrence(any occurrnce) of target
+        mid = low +(high-low)//2
+        if nums[mid]==target:#exiting for speed
+            occurrence = mid
+            break
+        if nums[mid]>target:
+            high = mid-1
+        else: low = mid +1
+    if occurrence ==-1:
+        return [-1,-1]
+    tHigh = high#To remember where  the max possible index for target is
+    
+    
+    high = occurrence
+    while low <high:#Find first occurrence
+        mid = low +(high-low)//2
+        if nums[mid]>=target:
+            high = mid
+        else: low = mid+1
+    first_occurrence = low#To remember the first occurrence
+
+    low = occurrence
+    high = tHigh
+    last_occurrence = occurrence
+    while low<=high:#find last occurence
+        mid = low +(high-low)//2
+        if nums[mid] == target:# in nums[low] can't be a number lower than target so no need to check
+            last_occurrence = mid
+            low = mid+1
+        else: high = mid-1
+    return [first_occurrence,last_occurrence]
