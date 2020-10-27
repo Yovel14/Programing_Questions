41cab689ecb4d166b886ddfd2c706c0c77950cbe
Yovel14 <yovelavitan14@gmail.com>
2020-10-27 18:52:21 +0200

    finnished Problem7

---
+++ Problem7.py
@@ -0,0 +1,42 @@
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+'''
+from math import sqrt
+
+
+def find_the_nth_Prime(n):
+    if n<=0: return -1
+    if n==1: return 2
+    if n==2: return 3
+    
+    
+    PrimeLs = [2,3]
+    n-=2# I already added 2 in the Prime Ls
+    current = 1#index for 6n+-1(all primes but 2,3 are of the form 6n+1 or 6n-1)
+    
+    def isPrime(a):#check for primes previus to a
+        sq = sqrt(a)#math.sqrt is faster than a**0.5
+        for i in PrimeLs:
+            if i >sq:#the largest Prime factor of a number is its square root
+                return True
+            if a%i==0: return False
+        return True
+    
+    while n!=0:
+        t = 6*current - 1
+        if isPrime(t):
+            PrimeLs.append(t)
+            n-=1
+            if n==0: break
+        
+        
+        t = 6*current + 1
+        if isPrime(t):
+            PrimeLs.append(t)
+            n-=1
+        current+=1
+    return t
+
+print(find_the_nth_Prime(10001))
