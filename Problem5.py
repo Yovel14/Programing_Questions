9969c927c615b1e08e4eae827c35dcbea4bf15d1
Yovel14 <yovelavitan14@gmail.com>
2020-10-27 14:19:34 +0200

    finnished Problem5

--- Problem5.py
+++ Problem5.py
@@ -16,6 +16,6 @@ for i in range(2,21):
             pcount+=1
         dic[j] = max(dic.get(j,0),pcount)
 ans = 1
-for k,v in dic.items():#get the min
+for k,v in dic.items():#get the minmultiple devisor
     ans*=(k**v)
 print(ans)
