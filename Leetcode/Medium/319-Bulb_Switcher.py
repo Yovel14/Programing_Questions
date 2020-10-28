c67d890b5d8f681e8046ff7ea1cd51fb2816ace0
Yovel14 <yovelavitan14@gmail.com>
2020-10-28 15:01:21 +0200

    .

--- Leetcode/Medium/319-Bulb_Switcher.py
+++ Leetcode/Medium/319-Bulb_Switcher.py
@@ -20,26 +20,27 @@ So you should return 1, because there is only one bulb is on.
 '''
 '''
 Idea:
-    one observation:
-        is that if we think about which lights will be change there status
-        the lights will change there status are lights which we turn odd number of times
-        to find the which lights we change odd number of times
-        we change a light when light place is  a multiple of another number `a` as `a` is `1....n`
-        which means only lights with odd number of devisors will be on at the end
-    
-    second observation:
-        numbers with odd number of devisors have a whole square root
+    One observation:
+        is that if we think about which lights will be changing their status
+        the lights will change there status are lights which we turn an odd number of times
+        to find which lights we change an odd number of times
+        we change a light when the light’s place is  a multiple of another number `a` as `a` is `1....n`
+        which means only lights with an odd number of divisors will be on at the end
+
+    Second observation:
+        numbers with an odd number of divisors have a whole square root
         Explanation:
-            the square root of a number is multiplied by it self thus it has an odd amount of devisors because we can devide each multple into pairs
-    
+            the square root of a number is multiplied by itself thus it has an odd amount of devisors because we can divide each multiple into pairs
+
     third observation:
-        the amount of numbers below n with odd number of devisors is sqrt of n and round down to a whole number
+        the amount of numbers below n with an odd number of divisors is sqrt of n and round down to a whole number
         Explanation:
             we need to count the square roots below n
             1^2+2^2+3^2+5^2.... = 1,4,9,14,25...
-            
-            if we want to generate all the pefect squares below n we run from 1 to sqrt of n and round down to a whole number
+
+            if we want to generate all the perfect squares below n we run from 1 to sqrt of n and round down to a whole number
             but since we just want the amount we can just take the sqrt of n and round down to a whole number
+
 '''
 
 from math import sqrt#math.sqrt is faster than n**0.5
