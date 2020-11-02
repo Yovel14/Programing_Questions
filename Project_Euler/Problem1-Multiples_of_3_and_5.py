61b63d815dcf03c504611c52ac643d8c30ee58b7
Yovel14 <yovelavitan14@gmail.com>
2020-11-02 06:33:24 +0200

    .

--- Project_Euler/Problem1-Multiples_of_3_and_5.py
+++ Project_Euler/Problem1-Multiples_of_3_and_5.py
@@ -4,11 +4,19 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 (https://projecteuler.net/problem=1)
 '''
-
-def sol(n):
+# using the formula of sum of numbers from a_1 to a_n 
+def sol1(n):
     n-=1
     n3 = n//3
     n5 = n//5
     n15 = n//15
     return (n3*(3+3*n3))//2+(n5*(5+5*n5))//2-(n15*(15+15*n15))//2
-print(sol(1000))
+print(sol1(1000))
+
+# using brute Force
+def sol2(n):
+    Sum = 0
+    for i in range(n):
+        if i%3 ==0 or i%5==0:
+            Sum+=i
+    return Sum
