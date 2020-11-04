43e3ba9ad22dc8c9132590354c405eceb42206f6
Yovel14 <yovelavitan14@gmail.com>
2020-11-04 04:02:35 +0200

    .

--- Project_Euler/Problem3-Largest_prime_factor.py
+++ Project_Euler/Problem3-Largest_prime_factor.py
@@ -5,14 +5,6 @@ What is the largest prime factor of the number 600851475143 ?
 (https://projecteuler.net/problem=3)
 '''
 
-from sympy import isprime
-from math import sqrt
-def LargestPrimeFactor(n):
-    if isprime(n):
-        return -1
-    sq = int(sqrt(n))
-    for i in range(sq if sq%2==1 else sq-1,2,-2):#sq if sq%2==1 else sq-1
-        if n%i==0 and isprime(i):
-            return i
-    return -1
-print(LargestPrimeFactor(600851475143))
+def Find_Largest_Prime_Factor(n):
+    lf = -1# largest factor
+    
