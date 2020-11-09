01be4ae0628403ceeafd36bed090402efa72fb65
Yovel14 <yovelavitan14@gmail.com>
2020-11-09 08:07:57 +0200

    Done

---
+++ Leetcode/Easy/746-Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,11 @@
+def minCostClimbingStairs(cost):
+    prev,current = cost[0],cost[1]
+    for n in cost[2:]:
+        prev, current = current,n+min(prev,current)
+    return min(prev,current)
+    
+    '''# useing the existing array
+    for i in range(2,len(cost)):
+        cost[i]+=min(cost[i-1],cost[i-2])
+    return min(cost[-1],cost[-2])
+    '''
