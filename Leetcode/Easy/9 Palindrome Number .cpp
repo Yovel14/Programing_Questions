186991bf77924c24ded43e30b4ffffd17a04ac1e
Yovel14 <yovelavitan14@gmail.com>
2020-12-11 21:00:18 +0200

    .

---
+++ Leetcode/Easy/9 Palindrome Number .cpp
@@ -0,0 +1,11 @@
+bool isPalindrome(int x) {
+	if(x<0 ||(x!=0 && x%10==0))return false;
+	int out = 0;// I will add to y each of the digit from x while removing it until y>=x (this method will avoid overflow)
+	// x will == y only if all char are the same 
+	while(x>out)
+	{
+		out = 10*out + x%10;
+		x/=10;
+	}
+	return (x==out || x== out/10);
+}
