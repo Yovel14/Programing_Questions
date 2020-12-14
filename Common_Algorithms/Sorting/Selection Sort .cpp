c2cdca8c2b4dcab222a320b34425c3ada4153ef0
Yovel14 <yovelavitan14@gmail.com>
2020-12-14 22:56:50 +0200

    .

---
+++ Common_Algorithms/Sorting/Selection Sort .cpp
@@ -0,0 +1,16 @@
+void Selection_Sort(vector<int> &v)// O(n^2)
+{
+	int n = v.size();
+	int min_index, temp;
+	for(int i = 0; i<n;++i)
+	{
+		min_index = i; 
+		for(int j = i + 1;j<n;++j)
+			if(v[j]<v[min_index])
+				min_index = j;
+		//swap v[i], v[min_index]
+		temp = v[min_index];
+		v[min_index] = v[i];
+		v[i] = temp;
+	}
+}
