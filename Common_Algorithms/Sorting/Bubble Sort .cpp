e4d455cd7c13a90e88b6650204cc56c33b43d014
Yovel14 <yovelavitan14@gmail.com>
2020-12-14 18:54:11 +0200

    .

---
+++ Common_Algorithms/Sorting/Bubble Sort .cpp
@@ -0,0 +1,21 @@
+void Bubble_Sort(vector<int> &v) // O(n^2) because doing n
+{	
+	int n = v.size();
+	bool isSorted = false;
+	int temp;
+	while(!isSorted)// loop will be at most O(n)
+	{
+		isSorted = true;
+		for(int i = 0;i<n-1;++i)// loop is done O(n)
+		{
+			if(v[i]>v[i+1])
+			{// swap v[i] ,v[i+1]
+				isSorted = false;
+				temp = v[i+1];
+				v[i+1] = v[i];
+				v[i] = temp;
+			}
+		}
+	}
+	// v is a refrence no need to return it
+}
---
+++ Common_Algorithms/Sorting/Insertion Sort .cpp
@@ -0,0 +1,15 @@
+void Insertion_Sort(vector<int> &v)// O(n^2)
+{
+	int n = v.size();
+	int val;
+	int j;
+	for(int i = 1; i<n;++i)
+	{
+		val = v[i];
+		for(j = i-1; j>=0 && v[j]>val;--j)
+		{
+			v[j+1] = v[j];
+		}
+		v[j+1] = val;
+	}
+}
