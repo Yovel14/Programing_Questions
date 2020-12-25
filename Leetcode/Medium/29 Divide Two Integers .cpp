288e19cd90c971a2954aabb1f46c0509929f2acc
Yovel14 <yovelavitan14@gmail.com>
2020-12-25 10:47:15 +0200

    Added new solution

--- Leetcode/Medium/29 Divide Two Integers .cpp
+++ Leetcode/Medium/29 Divide Two Integers .cpp
@@ -1,7 +1,8 @@
-int divide(int &dividend, int &divisor) // note divisor != 0
+int divide(int dividend, int divisor) // note divisor != 0
 {
-	if ((dividend == -2147483648 && divisor == -1)) // overflow
+	if ((dividend == -2147483648 && divisor == -1)) //overflow
 		return 2147483647;
+
 	if (dividend == 0)
 		return 0;
 	if (divisor == 1)
@@ -11,39 +12,25 @@ int divide(int &dividend, int &divisor) // note divisor != 0
 	if ((divisor & (1 << 31)) != (dividend & (1 << 31))) // do they have the same sign (positive or negitive)
 		negitive = true;
 
-	if (divisor == -2147483648) // this if is based on the fact that INT_MAX =  2147483647 while INT_MIN = -2147483648
-	{
-		if (dividend != divisor)
-			return 0;
-		return 1;
-	}
+	// converting divisor and dividend to negitive is to avoid overflow if one of them is equal to INT_MIN
+	if (divisor > 0)
+		divisor = ~divisor + 1;
+	if (dividend > 0)
+		dividend = ~dividend + 1;
 
 	int count = 0;
-
-	if (divisor < 0)
-		divisor = (~divisor) + 1;
-	if (dividend < 0)
-	{
-		if (dividend == -2147483648)
-		{
-			dividend += divisor; // if I will not add divisor when I convert divedend to positive it will overflow
-			count++;
-		}
-		dividend = (~dividend) + 1;
-	}
-
-	int adding = 1; // how much to add to count each round
-	int reducer;	// how much to add to remove from dividend
-	while (dividend >= divisor)
+	int adding;	 // how much to add to count each time
+	int reducer; // how much to remove from dividend
+	while (dividend <= divisor)
 	{
 		adding = 1;
 		reducer = divisor;
-		while (dividend >= reducer)
+		while (dividend <= reducer)
 		{
 			dividend -= reducer;
 			count += adding;
 			adding += adding;
-			if (reducer < 1073741823)
+			if (reducer >= -1073741824) // to avoid overflow
 				reducer += reducer;
 		}
 	}
