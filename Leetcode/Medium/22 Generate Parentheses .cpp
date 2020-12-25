18701f9e4df99ced089126cea7b043db2e8f7e7d
Yovel14 <yovelavitan14@gmail.com>
2020-12-25 04:15:05 +0200

    Added new answer

--- Leetcode/Medium/22 Generate Parentheses .cpp
+++ Leetcode/Medium/22 Generate Parentheses .cpp
@@ -1,3 +1,4 @@
+// solution 1
 void solve(int &n, int count_open, int count_closed, string current, vector<string> &com)
 {
 	if (count_open == n && count_closed == n)
@@ -16,4 +17,18 @@ vector<string> generateParenthesis(int &n)
 	vector<string> com;
 	solve(n, 0, 0, "", com);
 	return com;
+}
+
+// solution 2
+vector<string> generateParenthesis(int n) // there might be a possibility to use memozation to reduce time complexity in this solution
+{
+	if (n == 0)
+		return {""};
+
+	vector<string> ans;
+	for (int c = 0; c < n; c++)
+		for (string left : generateParenthesis(c))
+			for (string right : generateParenthesis(n - 1 - c))
+				ans.push_back("(" + left + ")" + right);
+	return ans;
 }
