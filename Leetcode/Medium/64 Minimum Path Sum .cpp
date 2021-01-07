bc331a6234d50c33b3750f9a0d82bb88b564996b
Yovel14 <yovelavitan14@gmail.com>
2021-01-07 17:24:21 +0200

    Added new solution

--- Leetcode/Medium/64 Minimum Path Sum .cpp
+++ Leetcode/Medium/64 Minimum Path Sum .cpp
@@ -5,9 +5,8 @@ int minPathSum(vector<vector<int>> &grid)
 
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
-		{
 			if (i != 0 || j != 0)
 				grid[i][j] += min((i != 0 ? grid[i - 1][j] : INT_MAX), (j != 0 ? grid[i][j - 1] : INT_MAX));
-		}
+
 	return grid[n - 1][m - 1];
 }
