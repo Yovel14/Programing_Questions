ada772c9e5927c00642fc84c1a26b94aef459c22
Yovel14 <yovelavitan14@gmail.com>
2021-01-11 20:48:03 +0200

    Added new solution

--- Leetcode/Easy/88 Merge Sorted Array .cpp
+++ Leetcode/Easy/88 Merge Sorted Array .cpp
@@ -10,8 +10,6 @@ void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
 			nums1[total_index--] = nums2[i2--];
 	}
 
-	while (i1 >= 0)
-		nums1[total_index--] = nums1[i1--];
 	while (i2 >= 0)
 		nums1[total_index--] = nums2[i2--];
 }
