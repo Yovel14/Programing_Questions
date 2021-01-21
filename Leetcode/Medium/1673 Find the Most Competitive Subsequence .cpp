eaeb2b2a360c934adcef19df9ea9bfc23b4f432f
Yovel14 <yovelavitan14@gmail.com>
2021-01-21 11:35:21 +0200

    Added new solution

---
+++ Leetcode/Medium/1673 Find the Most Competitive Subsequence .cpp
@@ -0,0 +1,15 @@
+vector<int> mostCompetitive(vector<int> &nums, int k)
+{
+	if (k == nums.size())
+		return nums;
+	int n = nums.size();
+	vector<int> ans;
+	for (int i = 0; i < n; i++)
+	{
+		while (ans.size() && nums[i] < ans.back() && ans.size() + n - i > k)
+			ans.pop_back();
+		if (ans.size() < k)
+			ans.push_back(nums[i]);
+	}
+	return ans;
+}
