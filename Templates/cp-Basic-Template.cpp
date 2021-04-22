68213e42e7903b5838f3644b07409384865b91cc
Yovel14 <yovelavitan14@gmail.com>
2021-04-22 19:49:50 +0300

    Templates

---
+++ Templates/cp-Basic-Template.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string.h>
+#include <cmath>
+#include <limits.h>
+#include <algorithm>
+#include <utility>
+
+#define FastIO                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);                   \
+	cout.tie(0);
+
+// Special types
+template <typename T>
+using vc = std::vector<T>;
+template <class T>
+using vc = std::vector<T>;
+using vi = std::vector<int>;
+using pii = std::pair<int, int>;
+using ll = long long;
+
+using namespace std;
+
+const int MOD = 1000000007;
+
+void inline preCampute() {}
+
+void inline solve()
+{
+}
+
+main()
+{
+	FastIO;
+
+	preCampute();
+
+	int ctc = 1;
+	//cout << "\n\n";
+	cin >> ctc;
+	while (ctc--)
+	{
+		solve();
+	}
+}
